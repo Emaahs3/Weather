1822b69a9030ba656bc404b73082e64b45c24324
Emaahs3 <estherahossi3@gmail.com>
2023-08-31 22:18:16 +0000

    provide  rain

--- my_weather/lib/main.dart
+++ my_weather/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_weather/constants/modeltheme.dart';
 import 'package:my_weather/screens/register.dart';
+import 'package:provider/provider.dart';
+
+import 'constants/constant.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,10 +14,46 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(),
-      debugShowCheckedModeBanner: false,
-      home: const Register(),
-    );
+    return ChangeNotifierProvider(
+        create: (_) => ModelTheme(),
+        child: Consumer<ModelTheme>(
+          builder: (context, ModelTheme themeNotifier, child) {
+            debugPrint(themeNotifier.isDark.toString());
+
+            return MaterialApp(
+                appBar: AppBar(
+                  actions: [
+                    IconButton(
+                      onPressed: () {
+                        themeNotifier.isDark = !themeNotifier.isDark;
+                        debugPrint(themeNotifier.isDark.toString());
+                      },
+                      icon: const Icon(Icons.dark_mode),
+                      color: Constant.blackColor,
+                    )
+                  ],
+                  backgroundColor: Constant.whiteColor,
+                  elevation: 0,
+                  leading: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Register()));
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_rounded,
+                        color: Constant.blackColor,
+                      )),
+                ),
+                theme: themeNotifier.isDark
+                    ? ThemeData(brightness: Brightness.light)
+                    : ThemeData(brightness: Brightness.dark),
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                  body: const Register(),
+                ));
+          },
+        ));
   }
 }
--- my_weather/lib/screens/home.dart
+++ my_weather/lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_weather/constants/constant.dart';
 import 'package:my_weather/screens/register.dart';
 import 'package:provider/provider.dart';
+import 'package:my_weather/constants/modeltheme.dart';
 
 class HomeWeather extends StatefulWidget {
   const HomeWeather({super.key});
@@ -16,188 +17,215 @@ class _HomeWeatherState extends State<HomeWeather> {
   String listCity = "Calavi";
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.dark_mode),
-              color: Constant.blackColor,
-            )
-          ],
-          backgroundColor: Constant.whiteColor,
-          elevation: 0,
-          leading: IconButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const Register()));
-              },
-              icon: const Icon(
-                Icons.arrow_back_rounded,
-                color: Constant.blackColor,
-              )),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(
-            top: 32.0,
-            left: 10.0,
-            right: 8.0,
-          ),
-          child: Column(
-            children: [
-              Row(),
-              TextButton(
-                  onPressed: () {
-                    showModalBottomSheet(
-                        isDismissible: true,
-                        context: context,
-                        builder: (BuildContext context) {
-                          return ListView.builder(
-                            itemCount: 25,
-                            itemBuilder: (context, index) {
-                              return Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  TextButton(
-                                    child: Text(
-                                      listCity,
-                                      style: const TextStyle(
-                                          color: Color.fromARGB(
-                                              255, 138, 141, 142)),
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        dismissible = true;
-                                        city = listCity;
-                                      });
-                                    },
-                                  ),
-                                  const Divider(
-                                    thickness: 2,
-                                    height: 0,
-                                  )
-                                ],
-                              );
-                            },
-                          );
+    return ChangeNotifierProvider(
+        create: (_) => ModelTheme(),
+        child: Consumer<ModelTheme>(
+          builder: (context, ModelTheme themeNotifier, child) {
+            return SafeArea(
+              child: Scaffold(
+                appBar: AppBar(
+                  actions: [
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          themeNotifier.isDark = !themeNotifier.isDark;
+                          debugPrint(themeNotifier.isDark.toString());
                         });
-                  },
-                  child: Text(
-                    city,
-                    style: const TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.w300),
-                  )),
-              const Padding(
-                padding: EdgeInsets.all(14.0),
-                child: Text(
-                  "Monday 7:00 AM",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  "assets/sun.png",
-                  width: 200,
-                  height: 200,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "22°C",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Text(
-                  "Good morning",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Text(
-                  "WASIM",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                ),
-              ),
-              const Divider(
-                indent: 140,
-                endIndent: 140,
-                height: 20,
-                thickness: 2,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 22.0, left: 10, right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: const [
-                        Icon(Icons.sunny),
-                        Text(
-                          "sunrise",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w300),
-                        ),
-                        Text(
-                          "7:00",
-                          style: TextStyle(
-                              fontSize: 26, fontWeight: FontWeight.w300),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      width: 2,
-                      height: 30,
-                      color: Colors.grey,
-                    ),
-                    Column(
-                      children: const [
-                        Icon(Icons.thermostat),
-                        Text(
-                          "wind",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w300),
-                        ),
-                        Text(
-                          "4m/s",
-                          style: TextStyle(
-                              fontSize: 26, fontWeight: FontWeight.w300),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      width: 2,
-                      height: 30,
-                      color: Colors.grey,
-                    ),
-                    Column(
-                      children: const [
-                        Icon(Icons.cloud),
-                        Text(
-                          "Temperature",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w300),
-                        ),
-                        Text(
-                          "23°",
-                          style: TextStyle(
-                              fontSize: 26, fontWeight: FontWeight.w300),
-                        ),
-                      ],
+                      },
+                      icon: const Icon(Icons.dark_mode),
+                      color: Constant.blackColor,
                     )
                   ],
+                  backgroundColor: Constant.whiteColor,
+                  elevation: 0,
+                  leading: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Register()));
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_rounded,
+                        color: Constant.blackColor,
+                      )),
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                body: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 32.0,
+                    left: 10.0,
+                    right: 8.0,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(),
+                      TextButton(
+                          onPressed: () {
+                            showModalBottomSheet(
+                                isDismissible: true,
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return ListView.builder(
+                                    itemCount: 25,
+                                    itemBuilder: (context, index) {
+                                      return Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          TextButton(
+                                            child: Text(
+                                              listCity,
+                                              style: const TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 138, 141, 142)),
+                                            ),
+                                            onPressed: () {
+                                              setState(() {
+                                                dismissible = true;
+                                                city = listCity;
+                                              });
+                                            },
+                                          ),
+                                          const Divider(
+                                            thickness: 2,
+                                            height: 0,
+                                          )
+                                        ],
+                                      );
+                                    },
+                                  );
+                                });
+                          },
+                          child: Text(
+                            city,
+                            style: const TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w300),
+                          )),
+                      const Padding(
+                        padding: EdgeInsets.all(14.0),
+                        child: Text(
+                          "Monday 7:00 AM",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          "assets/sun.png",
+                          width: 200,
+                          height: 200,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          "22°C",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: Text(
+                          "Good morning",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: Text(
+                          "WASIM",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                      const Divider(
+                        indent: 140,
+                        endIndent: 140,
+                        height: 20,
+                        thickness: 2,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 22.0, left: 10, right: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: const [
+                                Icon(Icons.sunny),
+                                Text(
+                                  "sunrise",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                                Text(
+                                  "7:00",
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: 2,
+                              height: 30,
+                              color: Colors.grey,
+                            ),
+                            Column(
+                              children: const [
+                                Icon(Icons.thermostat),
+                                Text(
+                                  "wind",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                                Text(
+                                  "4m/s",
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: 2,
+                              height: 30,
+                              color: Colors.grey,
+                            ),
+                            Column(
+                              children: const [
+                                Icon(Icons.cloud),
+                                Text(
+                                  "Temperature",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                                Text(
+                                  "23°",
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+            ;
+          },
+        ));
   }
 }
